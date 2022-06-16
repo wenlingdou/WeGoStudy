81d79c382a594739afc19deba326b3bbe089bf43
Wenling Dou <1984wldou@gmail.com>
2022-06-16 13:49:41 -0700

    final scripts

--- WeGoStudy_tests.py
+++ WeGoStudy_tests.py
@@ -10,21 +10,22 @@ class PositiveTestCases(unittest.TestCase):
         methods.login()
         # methods.create_new_student() #  no more new students
         # methods.edit_student_details()
-        # # methods.create_new_application()  # don't need new applications
+        # methods.sort_by_dropdown_menu()
+        # methods.create_new_application()  # don't need new applications
         # methods.view_application_list()
-        # methods.create_referral()  # cannot delete referrals, don't make more. Doesnt work, (pop up window)
-        # methods.commissions()
-#         methods.sort_by_dropdown_menu()
+        # # methods.create_referral()  # cannot delete referrals, don't make more. Doesnt work, (pop up window)
+        methods.commissions()
         methods.filter_by_study_area()
         methods.filter_by_city()
         methods.filter_by_program()
         methods.schools()
         methods.schools_sort_by()
-        methods.application_page()
+        methods.add_my_favorite()
+        methods.connect_to_wegostudy()
+        methods.visit_college_website()
+        methods.tuition()
+        methods.apply_now()
+        methods.study_area_prog_lev()
+        # methods.application_page()
         methods.logout()
         methods.tearDown()
-
-#         system log -- add log recorder 
-# hudson.plugins.git.GitStatus - All
-# com.cloudbees.jenkins.GitHubWebHook - All
-# org.jenkinsci.plugins.github - All
